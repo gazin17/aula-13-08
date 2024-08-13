1ca79134cc76fc480f0b4526f5478c7013a816d1
gazin17 <gazinplay14@gmail.com>
2024-08-13 13:47:44 -0300

    Add files via upload

---
+++ dump.sqlite (2).sql
@@ -0,0 +1,15 @@
+-- TABLE
+CREATE TABLE alunos(
+    id int(3) unique,
+    nome varchar(30)NOT NULL,
+    idade varchar(3),
+    turma varchar (4)
+  );
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
